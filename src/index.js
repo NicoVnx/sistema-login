1626d151e16181da1bdadf778343527542df7436
nicov <nico.vnx07@gmail.com>
2021-09-09 10:38:43 -0300

    ajustes

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const server = express()
+const routes = require("./routes")
+const session = require("express-session")
+
+
+
+server.use(session({
+secret: "ndfgjnw9824",
+resave: true,
+saveUninitialized: true}))
+
+server.use(express.static("public"))
+
+server.use(express.urlencoded({ extended: false }))
+
+server.set("view engine", "ejs")
+
+server.use(routes)
+server.listen(3000, () => console.log("Server Rodando!"))
